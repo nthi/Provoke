9f37717be39f6390db2f5acb3cff486e840e8d3d
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-12-05 15:54:55 -0500

    Implement BaseRepository in all repos

--- Provoke/Repositories/PlaceholderRepository.cs
+++ Provoke/Repositories/PlaceholderRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Provoke.Repositories
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Provoke.Models;
+//using Provoke.Utils;
+using System.Linq;
+
+namespace Provoke.Repositories
 {
-    public class PlaceholderRepository
+    public class PlaceholderRepository: BaseRepository
     {
+        public PlaceholderRepository(IConfiguration configuration) : base(configuration) { }
     }
 }
--- Provoke/Repositories/UserRepository.cs
+++ Provoke/Repositories/UserRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Provoke.Repositories
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Provoke.Models;
+//using Provoke.Utils;
+using System.Linq;
+
+namespace Provoke.Repositories
 {
-    public class UserRepository
+    public class UserRepository: BaseRepository
     {
+        public UserRepository(IConfiguration configuration) : base(configuration) { }
     }
 }
