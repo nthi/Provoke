fa2abd06e8d65a1c2032f6bdd3e5b3e4e87441af
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-12-07 15:57:18 -0500

    Creating Provoke.js, creating Components and Managers folders, creating auth folder with Login.js and Register.js

---
+++ Provoke/client/src/Provoke.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+
+export const Provoke = () => {
+    return 
+    <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={
+			<Authorized>
+				<>
+					<NavBar />
+					<ApplicationViews />
+				</>
+			</Authorized>
+		} />
+
+    </Routes>
+}
