cb710bc43ff9db8f13fdd446e2d885d4566df038
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-12-09 13:22:36 -0500

    This adds a GetAll method in Placeholder repos and controller. Also sets up the QuoteQueue file where I will generate new quotes to display in draft creation view

---
+++ Provoke/Controllers/PlaceholderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Provoke.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Provoke.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaceholderController : ControllerBase
+    {
+        private readonly IPlaceholderRepository _placeholderRepository;
+
+        public PlaceholderController(IPlaceholderRepository placeholderRepository)
+        {
+            _placeholderRepository = placeholderRepository;
+        }
+        // GET: api/<PlaceholderController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_placeholderRepository.GetAll());
+        }
+
+        // GET api/<PlaceholderController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<PlaceholderController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // PUT api/<PlaceholderController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<PlaceholderController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Provoke/Repositories/IPlaceholderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Provoke.Models;
+using System.Collections.Generic;
+
+namespace Provoke.Repositories
+{
+    public interface IPlaceholderRepository
+    {
+        List<Placeholder> GetAll();
+    }
+}
